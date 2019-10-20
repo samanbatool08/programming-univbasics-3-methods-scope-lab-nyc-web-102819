712f39d0b86d2e014e12f265f899e14d28e1fb7e
saman batool <samanbatool08@gmail.com>
2019-10-20 10:35:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,12 +1,3 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
-
-def toadstool
-  puts status
-end
-
 def link
   local_variable = "It's Dangerous To Go Alone! Take This."
   puts local_variable
